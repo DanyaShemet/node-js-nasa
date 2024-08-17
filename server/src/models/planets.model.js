4cf3d83233ea41e50a5f48c301bda782e18d00e0
DanyaShemet <shemetg@gmail.com>
2024-08-17 18:26:48 +0300

    tests: mongoose save planets

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -2,7 +2,7 @@ const { parse } = require('csv-parse')
 const path = require('path')
 const fs = require('fs')
 
-const habitablePlanets = []
+const planets = require('./planets.mongo')
 
 function isHabitablePlanet(planet) {
     return (
@@ -24,26 +24,49 @@ function loadPlanetsData() {
                     columns: true,
                 })
             )
-            .on('data', (data) => {
+            .on('data', async (data) => {
                 if (isHabitablePlanet(data)) {
-                    habitablePlanets.push(data)
+                    await savePlanet(data)
                 }
             })
             .on('error', (err) => {
                 console.log(err)
                 reject(err)
             })
-            .on('end', () => {
-                console.log(
-                    `${habitablePlanets.length} habitable planets found!`
-                )
+            .on('end', async () => {
+                const countPlanetsFound = (await getAllPlanets()).length
+                console.log(`${countPlanetsFound} habitable planets found!`)
                 resolve()
             })
     })
 }
+async function getAllPlanets() {
+    return await planets.find(
+        {},
+        {
+            _id: 0,
+            __v: 0,
+        }
+    )
+}
 
-function getAllPlanets() {
-    return habitablePlanets
+async function savePlanet(planet) {
+    console.log(planet.kepler_name)
+    try {
+        await planets.updateOne(
+            {
+                keplerName: planet.kepler_name,
+            },
+            {
+                keplerName: planet.kepler_name,
+            },
+            {
+                upsert: true,
+            }
+        )
+    } catch (error) {
+        console.error(`Could not save planet ${error}`)
+    }
 }
 
 module.exports = {
